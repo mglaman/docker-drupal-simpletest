2e278ed35dbb73038a151882ad4f14ce77e66240
Matt Glaman <nmd.matt@gmail.com>
2016-03-16 15:05:21 -0500

    Add more verbose options fo rrunning.

--- README.md
+++ README.md
@@ -8,5 +8,11 @@ docker build 7 -t simpletest-php7
 docker run -it --rm simpletest-php7 -v /path/to/drupal:/app
 
 docker build 5.6 -t simpletest-php5.6
-docker run -it --rm simpletest-php5.6 -v /path/to/drupal:/app
+docker run -it --rm simpletest-php5.6bec -v /path/to/drupal:/app
+````
+
+If using the `drupal-composer/drupal-project` or `drupalcommerce/project-base`
+
+````sh
+docker run -it --rm -v $(pwd):/app --env SIMPLETEST_SCRIPT="/app/web/core/scripts/run-tests.sh" --env PHP_DOCROOT="/app/web" simpletest-php7
 ````
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
-php -S ${PHP_SERVER} -t /app > /dev/null 2>&1 &
+php -S ${PHP_SERVER} -t ${PHP_DOCROOT} > /dev/null 2>&1 &
 
 sleep 2
 
 curl -I ${SIMPLETEST_BASE_URL}
 
-php /app/core/scripts/run-tests.sh \
-    --concurrency 5 \
-    --url ${SIMPLETEST_BASE_URL} \
-    --dburl ${SIMPLETEST_DB} \
-    --sqlite /tmp/tmp.sqlite \
-    --color \
-    ${SIMPLETEST_GROUP}
+echo "php ${SIMPLETEST_SCRIPT} --concurrency 5 --url ${SIMPLETEST_BASE_URL} --dburl ${SIMPLETEST_DB} --sqlite /dev/shm/tmp.sqlite --color ${SIMPLETEST_GROUP} | tee simpletest.log"
+php ${SIMPLETEST_SCRIPT} --concurrency 5 --url ${SIMPLETEST_BASE_URL} --dburl ${SIMPLETEST_DB} --sqlite /tmp/tmp.sqlite --color ${SIMPLETEST_GROUP} | tee simpletest.log
