c0aa36dbd5508ba148ed7211d3ba67818fbe7ef9
Matt Glaman <nmd.matt@gmail.com>
2016-03-16 16:56:24 -0500

    Update sqlite storage location.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ sleep 2
 
 curl -I ${SIMPLETEST_BASE_URL}
 
-echo "php ${SIMPLETEST_SCRIPT} --concurrency 5 --url ${SIMPLETEST_BASE_URL} --dburl ${SIMPLETEST_DB} --sqlite /dev/shm/tmp.sqlite --color ${SIMPLETEST_GROUP} | tee simpletest.log"
+echo "php ${SIMPLETEST_SCRIPT} --concurrency 5 --url ${SIMPLETEST_BASE_URL} --dburl ${SIMPLETEST_DB} --sqlite /tmp/tmp.sqlite --color ${SIMPLETEST_GROUP} | tee simpletest.log"
 php ${SIMPLETEST_SCRIPT} --concurrency 5 --url ${SIMPLETEST_BASE_URL} --dburl ${SIMPLETEST_DB} --sqlite /tmp/tmp.sqlite --color ${SIMPLETEST_GROUP} | tee simpletest.log
